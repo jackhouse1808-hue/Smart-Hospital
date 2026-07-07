378877bc2837c6ea9d3db8a4c08574df735c0981
The best striker of all time <jackhouse1808@gmail.com>
2026-07-07 12:02:08 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -5,3 +5,199 @@ import pickle
 with open("hospital_model_bryan.pkl","rb") as f:
   bundle = pickle.load(f)
   st.write("Connected")
+
+model = bundle["model"]
+scaler = bundle["scaler"]
+
+features = bundle["features"]
+cols_to_scale = bundle["cols_to_scale"]
+
+depth_map_inv = bundle["depth_map_inv"]
+
+gender_map = bundle["gender_map"]
+temp_map = bundle["temp_map"]
+hr_map = bundle["hr_map"]
+dur_map = bundle["dur_map"]
+cc_map = bundle["cc_map"]
+
+DEPT_INFO = {
+  "Respiratory Medicine": {
+    "icon" : "🫁",
+    "desc" : "Specializes is conditions affecting the lungs and airways",
+    "next" : [
+      "visit level 2, Wing B", 
+      "Estimated wait: 15-25 minutes", 
+      "Please wear mask"
+    ]
+  },
+   "Cardiology Medicine": {
+    "icon" : "❤️",
+    "desc" : "Specializes in heart and cardiovascular conditions",
+    "next" : [
+      "visit level 3, Wing A", 
+      "Estimated wait: 20-30 minutes", 
+      "Please bring previous ECG reports"
+    ]
+  },
+ "Gastroenterology Medicine": {
+    "icon" : "🫄",
+    "desc" : "Specializes in digestive system conditions",
+    "next" : [
+      "visit level 1, Wing C", 
+      "Estimated wait: 20-30 minutes"
+    ]
+  },
+"Neurology Medicine": {
+    "icon" : "🧠",
+    "desc" : "Specializes in brain and nervous system conditions",
+    "next" : [
+      "visit level 1, Wing C", 
+      "Estimated wait: 30 minutes - 60 minutes",
+      "Bring current medication list"
+    ]
+  },
+"General Medicine": {
+    "icon" : "🩺",
+    "desc" : "General Health Consultation",
+    "next" : [
+      "visit level 1, Wing A"
+    ]
+  },
+"Dermatology Medicine": {
+    "icon" : "👩",
+    "desc" : "Specializes in skin conditions",
+    "next" : [
+      "visit level 2, Wing D"
+    ]
+  }
+}
+
+st.title("🏥 Smart Hospital Patient Navigator Bryan")
+
+st.write("Fill in the patient's information below")
+
+st.header("Patient Information")
+
+age = st.number_input(
+  "Age",
+  min_value=1,
+  max_value=120,
+  value=30
+)
+gender = st.selectbox(
+  "Gender", 
+  ["Female", "Male"]
+)
+
+st.header("Symptoms")
+
+col1, col2 = st.columns(2)
+
+with col1: 
+  fever = st.checkbox("Fever 🥵")
+  cough = st.checkbox("Cough 😷")
+  headache = st.checkbox("Headache 💆‍♂️")
+  chest_pain = st.checkbox("Chest Pain 💔")
+  stomach_pain = st.checkbox("Stomach Pain 🤢")
+
+with col2: 
+  shortness_breath = st.checkbox("Shortness of Breath 😮‍💨")
+  nausea_vomiting = st.checkbox("Nausea/Vomiting 🤮")
+  dizziness = st.checkbox("Dizziness 😵‍💫")
+  skin_rash = st.checkbox("Skin Rash 🔴")
+
+st.header("Patient Condition")
+temperature_level = st.selectbox(
+  "Temperature", 
+  option = list(temp_map.keys())
+)
+
+heart_rate_level = st.selectbox(
+  "Heart Rate", 
+  option = list(hr_map.keys())
+)
+
+duration = st.selectbox(
+  "Duration of Symptoms", 
+  option = list(dur_map.keys())
+)
+
+chief_complaint = st.selectbox(
+  "Chief Complaint", 
+  option = list(cc_map.keys())
+)
+
+st.header("Medical History")
+hypertension = st.checkbox("Hypertension")
+
+heart_disease = st.checkbox("Heart Disease")
+
+asthma = st.checkbox("Asthma")
+
+predict_button = st.button("Predict Department")
+
+if predict_button:
+  patient = pd.DataFrame ([{
+    "age" : age, 
+    "gender" : gender_map[gender], 
+    "fever" : int(fever), 
+    "cough" : int(cough), 
+    "headache" : int(headache), 
+    "chest_pain" : int(chest_pain), 
+    "stomach_pain" : int("stomach_pain"),
+    "shortness_breath" : int("shortness_breath")
+    "nausea_vomiting" : int("nausea_vomiting")
+    "dizziness" : int("dizziness")
+    "skin_rash" : int("skin_rash")
+
+    "temperature_level": temp_map[temperature_level], 
+    "heart_rate_level": hr_map[heart_rate_level],
+    "duration": dur_map[duration],
+
+    "asthma" : int(asthma),
+    "hypertension" : int(hypertension),
+    "heart_disease" : int(heart_disease),
+
+    "chief_complaint" : cc_map[chief_complaint]
+  }])
+
+patient_scaled = patient.copy()
+
+patient_scaled[cols_to_scale] = scaler.transform(
+  patient[cols_to_scale]
+)
+
+prediction = model.predict(
+  patient_scaled[features]
+)[0]
+
+probability = model.predict_proba(
+  patient_scaled[features]
+)[0]
+
+departments = dept_map_inv[prediction]
+
+confidence = probability[prediction] * 100
+
+st.divider()
+st.header("Prediction Result")
+info = DEPT_INFO.get(department)
+
+if info:
+
+  st.success(
+    f"{info["icon"]} : recommended Department: {Department}"
+  )
+  st.write(f"**Confidence** {confidence: }%")
+  st.write("Description: ")
+  st.write(info["desc"])
+  st.write("What should the patient do?")
+
+  for step in info["next"]: 
+    st.write(f"{step}")
+
+else:
+  st.success(f"Recommended Department: {department}")
+  st.write(f"Confidence : {confidence}")
+
+st.warning("This A.I. Recommendation is only for educational purposes")
